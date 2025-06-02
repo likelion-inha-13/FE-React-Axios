c5e177be3f11e5f8c31af03817c19d34c0bba0a7
leedasom050115 <kiss9892@inha.edu>
2025-06-02 16:46:41 +0900

    과제제출입니다

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -3,24 +3,63 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "../styles/LoginPage.css";
 
-function LoginForm() {
-  
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
+    try {
+      const response = await axios.post(
+        "https://reqres.in/api/login",
+        {
+          email: email,
+          password: password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-key": "reqres-free-v1",
+          },
+        }
+      );
 
-    
+      const { token } = response.data;
+
+      localStorage.setItem("email", email);
+      localStorage.setItem("token", token);
+
+      navigate("/userlist");
+    } catch (error) {
+      if (error.response && error.response.data && error.response.data.error) {
+        alert(error.response.data.error);
+      } else {
+        alert("로그인 실패");
+      }
+      console.error(error);
+    }
   };
 
   return (
     <form onSubmit={handleLogin} className="login-container">
       <h2>🔐 로그인</h2>
-      <input/>
-      <input/>
+      <input
+        type="email"
+        required
+        placeholder="이메일"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        required
+        placeholder="비밀번호"
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button type="submit">로그인</button>
     </form>
   );
 }
 
-export default LoginForm;
+export default LoginPage;
